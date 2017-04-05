0b58593c43dccbe3b19181d502d1d4bbd4ee1a56
Formacion850 <Formacion850@stkproj.com>
2017-04-05 14:15:32 +0200

    componentes a mazo

---
+++ src/app/entradas/entradas.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EntradasComponent } from './entradas.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EntradasComponent]
+})
+export class EntradasModule { }
---
+++ src/app/vinilos/vinilos.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VinilosComponent } from './vinilos.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [VinilosComponent]
+})
+export class VinilosModule { }
