f6e56406dd9c5cd9b571c2b17996017056be5335
Formacion850 <Formacion850@stkproj.com>
2017-04-05 13:46:34 +0200

    componentes creados

---
+++ src/app/discos/discos-routing.module.ts
@@ -0,0 +1,19 @@
+/** Módulos de enrutado de Angular2 */
+import { RouterModule, Routes } from '@angular/router';
+
+import { NgModule } from '@angular/core';
+
+// Array con las rutas de este módulo
+const routes: Routes = [
+  { path: '', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes) // configuración para un módulo raiz
+  ],
+  exports: [
+    RouterModule // se importará desde el módulo padre, el raíz
+  ]
+})
+export class SecurityRoutingModule { }
---
+++ src/app/discos/discos.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DiscosComponent } from './discos.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DiscosComponent]
+})
+export class DiscosModule { }
